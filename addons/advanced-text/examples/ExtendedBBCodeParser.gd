dc261cc4e5d8e6201e1bdb001f8c9ec73950cc7a
Jeremi Biernacki <jeremi360@gmail.com>
2023-09-27 12:45:39 +0200

    headers and base Parser finally starts to working

---
+++ addons/advanced-text/examples/ExtendedBBCodeParser.gd
@@ -0,0 +1,36 @@
+@icon("res://addons/advanced-text/icons/bbcode.svg")
+@tool
+extends TextParser
+class_name ExtendedBBCodeParser
+
+var headers : Array[int] = [22, 20, 18, 16]
+
+func parse(text:String) -> String:
+	text = parse_headers(text)
+	
+	# if emojis_gd:
+	# 	output = emojis_gd.parse_emojis(output)
+			
+	# if icons_gd:
+	# 	output = parse_icons(output)
+	
+	return text
+
+func parse_headers(text:String) -> String:
+	# replace [h1]text[/h1] with [font_size=22]text[/font_size]
+	# replace [h2]text[/h2] with [font_size=20]text[/font_size]
+	# replace [h3]text[/h3] with [font_size=18]text[/font_size]
+	# replace [h4]text[/h4] with [font_size=16]text[/font_size]
+
+	var re = RegEx.new()
+	re.compile("\\[h(?P<size>[1-4])\\](?P<text>.+?)\\[/h(?P=size)\\]")
+
+	var found := re.search(text)
+	if not found:
+		return text
+
+	var h_size := found.get_string("size").to_int()
+	var font_size := headers[h_size - 1]
+	text = text.replace("[h" + str(h_size) + "]", "[font_size=" + str(font_size) + "]")
+	text = text.replace("[/h" + str(h_size) + "]", "[/font_size]")
+	return text
---
+++ addons/advanced-text/nodes/AdvancedTextLabel.gd
@@ -0,0 +1,36 @@
+@tool
+@icon("res://addons/advanced-text/icons/AdvancedTextLabel.svg")
+extends RichTextLabel
+class_name AdvancedTextLabel
+
+@export_multiline var _text := "" :
+	get:
+		return _text
+	
+	set(value):
+		_text = value
+		update_text()
+
+@export var parser : TextParser:
+	get:
+		return parser
+	
+	set(value):
+		parser = value
+		if value != null:
+			update_text()
+
+func update_text():
+	bbcode_enabled = false
+	if parser == null:
+		return
+
+	if _text.is_empty():
+		return
+
+	bbcode_enabled = true
+	parse_bbcode(parser.parse(_text))
+
+	
+
+
