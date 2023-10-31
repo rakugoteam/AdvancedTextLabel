06ba5fca02675718ce76b5fb3b52f0d0da44d139
Jeremi Biernacki <jeremi360@gmail.com>
2023-10-31 22:44:24 +0100

    refactor AdvancedTextLabel setters to fix Stack overflow errors

--- addons/advanced-text/nodes/AdvancedTextLabel.gd
+++ addons/advanced-text/nodes/AdvancedTextLabel.gd
@@ -4,52 +4,38 @@ extends RichTextLabel
 class_name AdvancedTextLabel
 
 @export_file var text_file := "" :
-	get:
-		return text_file
-	
 	set(value):
 		text_file = value
-		update_text()
-
-@export_multiline var _text := "" :
-	get:
-		return _text
+		var f := FileAccess.open(text_file, FileAccess.READ)
+			
+		if f.get_open_error() == OK:
+			_text = f.get_as_text()	
+		f.close()
 	
+	get: return text_file
+
+@export_multiline var _text := "":
 	set(value):
 		_text = value
-		update_text()
+		_parse_text()
+	
+	get : return _text
 
 @export var parser : TextParser:
-	get:
-		return parser
-	
-	set(value):
-		parser = value
-		if value != null:
-			update_text()
-
-func _ready():
-	update_text()
-
-func update_text():
-	if !is_ready():
-		return
-	
-	bbcode_enabled = false
-	if parser == null:
-		return
-
-	if text_file:
-		var f := FileAccess.open(text_file, FileAccess.READ)
+	set (value):
+		if !is_ready():
+			return
 		
-		if f.get_open_error() == OK:
-			_text = f.get_as_text()
-			f.close()
+		parser = value
+		_parse_text()
 	
-	if _text.is_empty():
-		return
+	get: return parser
 
+func _parse_text() -> void:
 	bbcode_enabled = true
+
+	if !parser:
+		text = _text
+		return
+	
 	text = parser.parse(_text)
-
-
