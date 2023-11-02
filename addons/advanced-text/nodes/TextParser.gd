24165a5da3b35baa60b786c2c3bd237cff495cd3
Jeremi Biernacki <jeremi360@gmail.com>
2023-11-02 18:22:12 +0100

    add rest_parser() to base TextParser class

--- addons/advanced-text/nodes/TextParser.gd
+++ addons/advanced-text/nodes/TextParser.gd
@@ -13,7 +13,13 @@ var replacement := ""
 func parse(text:String) -> String:
 	return text
 
-## func that my parsers uses to replace result in given text with replacement.
+## Restores default parser settings
+## This only exits to be override by Parsers classes that inherits from it
+## In this calls is do nothing
+func reset_parser():
+	pass
+
+## Func that my parsers uses to replace result in given text with replacement.
 func replace_regex_match(text:String, result:RegExMatch, replacement:String) -> String:
 	var start_string := text.substr(0, result.get_start())
 	var end_string := text.substr(result.get_end(), text.length())
