b8d2f80208baf2ae9f76a4bc7615755c5a347495
Jeremi Biernacki <jeremi360@gmail.com>
2023-11-02 13:17:02 +0100

    Add docs comments to ExtendedBBCodeParser

--- addons/advanced-text/nodes/ExtendedBBCodeParser.gd
+++ addons/advanced-text/nodes/ExtendedBBCodeParser.gd
@@ -1,40 +1,47 @@
 @icon("res://addons/advanced-text/icons/bbcode.svg")
 @tool
 extends TextParser
-class_name ExtendedBBCodeParser
-
-## there must be 4 sizes of headers
-@export var headers : Array[int] = [22, 20, 18, 16]
-@export var custom_header_font : Font
-
-# TODO make possible for icons and emojis to change
-# TODO their tags so they can be more in sync with the text parser
 
 # ! Remember To release Icons and Emojis
 # ! with need fixes for Advanced Text,
 # ! before release this addon !
 
+## This parser adds Headers {h1}, :emojis: and icons [icon:name] add Rakugo variables with <var_name> to BBCode
+class_name ExtendedBBCodeParser
+
+## Sizes of headers
+## there must be 4 sizes of headers
+## by default sizes are: 22, 20, 18 and 16
+@export var headers : Array[int] = [22, 20, 18, 16]
+
+## If not null it will be used by headers
+@export var custom_header_font : Font
+
+## Safe way of getting EmojisDB singleton
 var emojis_db : Node :
 	get:
-		if has_node("/root/EmojisDB"):
-			return get_node("/root/EmojisDB")
+		if Engine.has_singleton("/root/EmojisDB"):
+			return Engine.get_singleton("/root/EmojisDB")
 
 		return null
 
+## Safe way of getting MaterialIconsDB singleton
 var icons_db : Node :
 	get:
-		if has_node("/root/MaterialIconsDB"):
-			return get_node("/root/MaterialIconsDB")
+		if Engine.has_singleton("/root/MaterialIconsDB"):
+			return Engine.get_singleton("/root/MaterialIconsDB")
 		
 		return null
 
+## Safe way of getting Rakugo singleton
 var rakugo : Node :
 	get:
-		if has_node("/root/Rakugo"):
-			return get_node("/root/Rakugo")
+		if Engine.has_singleton("/root/Rakugo"):
+			return Engine.get_singleton("/root/Rakugo")
 	
 		return null
 
+## Returns given text parsed to BBCode
 func parse(text:String) -> String:
 	text = parse_headers(text)
 	
@@ -49,18 +56,23 @@ func parse(text:String) -> String:
 	
 	return text
 
+## Restores default headers settings
 func reset_parser():
 	headers = [22, 20, 18, 16]
 	custom_header_font = null
 
+## Parse emojis in given text into BBCode
+## You need Emojis-For-Godot addon for this
 func parse_emojis(text:String) -> String:
 	return emojis_db.parse_emojis(text)
 
+## Parse icons in given text into BBCode
+## You need Godot-Material-Icons addon for this
 func parse_icons(text:String) -> String:
 	return icons_db.parse_icons(text)
 
+## Parse headers in given text into BBCode
 func parse_headers(text:String) -> String:
-	# replace [h1]text[/h1] with [font_size=22]text[/font_size]
 	re.compile("\\[h(?P<size>[1-4])\\](?P<text>.+?)\\[/h(?P=size)\\]")
 
 	result = re.search(text)
