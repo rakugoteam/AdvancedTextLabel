cef3bd8e0761d22df50d188ceec277fab078189e
Jeremi Biernacki <jeremi360@gmail.com>
2023-11-02 18:29:31 +0100

    improved handling of text files by AdvancedTextLabel

--- addons/advanced-text/nodes/AdvancedTextLabel.gd
+++ addons/advanced-text/nodes/AdvancedTextLabel.gd
@@ -1,6 +1,7 @@
 @tool
 @icon("res://addons/advanced-text/icons/AdvancedTextLabel.svg")
 extends RichTextLabel
+
 class_name AdvancedTextLabel
 
 @export_file var text_file := "" :
@@ -32,10 +33,16 @@ func _ready():
 	_parse_text()
 
 func _parse_text() -> void:
+	if !is_node_ready(): return
 	bbcode_enabled = true
 
 	if !parser:
 		text = _text
+		if !Engine.is_editor_hint():
+			push_warning("parser is null at " + str(get_path()))
+		else:
+			var root := Engine.get_main_loop().edited_scene_root as Node
+			push_warning("parser is null at " + str(root.get_path_to(self)))
 		return
 	
 	if text_file:
@@ -43,6 +50,10 @@ func _parse_text() -> void:
 		if f.get_open_error() == OK:
 			_text = ""
 			text = parser.parse(f.get_as_text())
+		
+		else:
+			push_error("can't load file: " + text_file)
+
 		f.close()
 		return
 	
