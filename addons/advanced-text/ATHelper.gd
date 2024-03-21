c2caf260a253948da72241208089aaff4dfd0466
Jeremi Biernacki <jeremi360@gmail.com>
2024-03-21 17:34:38 +0100

    fix get_singleton() works under Editor

--- addons/advanced-text/ATHelper.gd
+++ addons/advanced-text/ATHelper.gd
@@ -1,7 +1,15 @@
 @tool
 extends Node
 
-func get_singleton(name: String) -> Object:
-	if has_node("/root/%s" % name):
-		return get_node("/root/%s" % name)
+## This is a helper func to get user singletons outside of the main scene tree.
+## Used to get: Rakugo, EmojisDB and MaterialIconsDB
+func get_singleton(sname: String) -> Object:
+	var root := get_tree().root
+	
+	if Engine.is_editor_hint():
+		root = get_tree().edited_scene_root
+	
+	if root.has_node(sname):
+		return root.get_node(sname)
+	
 	return null
