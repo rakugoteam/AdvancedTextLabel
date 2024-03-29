e1efa3f8034e1956de76fb5827a41e521182de8f
Jeremi Biernacki <jeremi360@gmail.com>
2024-03-29 14:10:45 +0100

    connect with RakuVars

--- addons/advanced-text/nodes/AdvancedTextLabel.gd
+++ addons/advanced-text/nodes/AdvancedTextLabel.gd
@@ -5,6 +5,10 @@ extends RichTextLabel
 ## This class parses given text to bbcode using given TextParser
 class_name AdvancedTextLabel
 
+## Rakugo singleton reference
+## Used only if RDS addon is enabled
+var raku: Node
+
 ## By default links (staring from `http`) will be opened in web browser
 ## For custom links you can connect to `custom_link` signal
 signal custom_link(args)
@@ -53,6 +57,16 @@ func _parse_text() -> void:
 				
 			else:
 				parser.root = get_tree().root
+		
+		if parser.root:
+			if !raku:
+				if parser.root.has_node("Rakugo"):
+					raku = parser.root.get_node("Rakugo")
+			
+			if raku:
+				var sg = raku.sg_variable_changed
+				if !sg.is_connected(_on_rakuvars_changed):
+					sg.connect(_on_rakuvars_changed)
 	
 	if !parser:
 		push_warning("parser is null at " + str(name))
@@ -60,6 +74,9 @@ func _parse_text() -> void:
 		return
 	
 	text = parser.parse(_text)
+
+func _on_rakuvars_changed(var_name, value) -> void:
+	_parse_text()
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings: PackedStringArray = []
