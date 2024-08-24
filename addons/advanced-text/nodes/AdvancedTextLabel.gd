e80f1d0a0ed13523a38ee6da18f87a19208d4064
Jeremi Biernacki <jeremi360@gmail.com>
2024-08-24 14:25:49 +0200

    fix size of AdvancedText Nodes when add/remove them or when changing text in them

--- addons/advanced-text/nodes/AdvancedTextLabel.gd
+++ addons/advanced-text/nodes/AdvancedTextLabel.gd
@@ -30,21 +30,41 @@ signal custom_link(url:String)
 		parser = value
 		update_configuration_warnings()
 		if parser:
-			parser.changed.connect(_parse_text)
+			if not parser.changed.is_connected(_parse_text):
+				parser.changed.connect(_parse_text)
 
 			if parser is ExtendedBBCodeParser:
 				for h in parser.headers:
-					h.changed.connect(_parse_text)
+					if not h.changed.is_connected(_parse_text):
+						h.changed.connect(_parse_text)
 
 			_parse_text()
-			print("parse text")
+			# print("parse text")
 	
 	get: return parser
+
+var font_size : int:
+	get: return theme.get_font_size(get_class(), &"normal")
+
+
+var parent_org_size : Vector2
+func _enter_tree():
+	if get_parent_control():
+		parent_org_size = get_parent_control().size
+
+func _exit_tree():
+	if get_parent_control():
+		get_parent_control().size = parent_org_size
 
 func _ready():
 	bbcode_enabled = true
 	meta_clicked.connect(_on_meta)
+	if not _text:
+		custom_minimum_size = Vector2.ONE * font_size
+
 	_parse_text()
+	_exit_tree()
+
 
 func _parse_text() -> void:
 	if !is_node_ready(): return
@@ -62,6 +82,20 @@ func _parse_text() -> void:
 		return
 	
 	text = parser.parse(_text)
+	if not scroll_active:
+		var lines_number := (1 + _text.count('\n'))
+		var new_minimum_size := Vector2()
+		new_minimum_size.y = lines_number * font_size
+		var max_line_lenght := visible_characters
+		if visible_characters == -1:
+			for l : String in _text.split('\n'):
+				if l.length() > max_line_lenght:
+					max_line_lenght = l.length()
+		
+		new_minimum_size.x = max_line_lenght * font_size
+		if new_minimum_size > custom_minimum_size:
+			custom_minimum_size = new_minimum_size
+
 
 func _on_rakuvars_changed(var_name, value) -> void:
 	if "<%s>" % var_name in _text:
